e0fa4eb615fe3a0409b96d4659171d0830bf08f0
akhil-ibm <121149449+akhil-ibm@users.noreply.github.com>
2023-11-13 17:58:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,12 @@ def sample():
     data = cursor.fetchall()
     print(data)
 
+        
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+
+
     return data
 
 if __name__ == '__main__':
@@ -37,28 +43,4 @@ if __name__ == '__main__':
 
 
 
-# # Get the list of tables in the database
-# cursor.execute("SHOW TABLES")
 
-# tables = cursor.fetchall()
-# # 
-# # Iterate through the tables and get details for each
-# for table in tables:
-#     table_name = table[0]
-
-#     # Get the column names and data types for each table
-#     cursor.execute(f"DESCRIBE {table_name}")
-#     columns = cursor.fetchall()
-
-#     print(f"Table: {table_name}")
-#     print("Column Details:")
-#     for column in columns:
-#         print(f"  {column[0]} - {column[1]}")
-
-#     print("\n")
-
-
-
-# Close the cursor and connection
-cursor.close()
-connection.close()
